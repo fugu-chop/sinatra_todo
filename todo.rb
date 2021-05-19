1f99119153cee93e875ea2d3521722c7a45e5b30
Dean Wan <deanwan14@gmail.com>
2021-05-20 08:21:49 +1000

    Morning updates, 20th May

--- todo.rb
+++ todo.rb
@@ -1,11 +1,14 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/content_for'
+require 'tilt/erubis'
 
 # This sets up Sinatra to use sessions
 configure do
   enable(:sessions)
-  # We need to set the session secret here. 
+  # We need to set the session secret here.
   # If we don't specify a value here, Sinatra will randomly create a session secret every time it starts.
   # This means that every time Sinatra starts up again, a new secret will be generated, invalidating any old sessions.
   set(:session_secret, 'secret')
@@ -21,14 +24,15 @@ helpers do
   end
 
   def create_list
-    session[:lists] << { name: @list_name, todos: [] }
-    session[:success] = "The list has been created."
-    redirect "/lists"
+    @id = -1
+    session[:lists] << { id: @id += 1, name: @list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
   end
 
   def set_error_message
-    session[:error] = "The list name must be between 1 and 100 characters." if !valid_input?
-    session[:error] = "The list name must be unique." if duplicate_list_name?
+    session[:error] = 'The list name must be between 1 and 100 characters.' unless valid_input?
+    session[:error] = 'The list name must be unique.' if duplicate_list_name?
   end
 
   def duplicate_list_name?
@@ -36,27 +40,37 @@ helpers do
   end
 end
 
-get "/" do 
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
-# View all lists
-get "/lists" do
+get '/lists' do
   @lists = session[:lists]
   erb(:lists)
 end
 
-# Render the new list form
-get "/lists/new" do 
+get '/lists/new' do
   erb(:new_list)
 end
 
 # Create a new list
-post "/lists" do
+post '/lists' do
   # params[:list_name] comes from the name of the input field in our erb file
   # With a POST request, the name:value are captured as invisible query params within the response body
   @list_name = params[:list_name].strip
   return create_list if valid_input? && !duplicate_list_name?
+
   set_error_message
   erb(:new_list)
+end
+
+get '/lists/:id' do
+  # Params are passed as strings from Sinatra
+  idx = params[:id].to_i
+  @list = session[:lists][idx]
+  erb(:list)
+end
+
+get '/lists/:id/edit' do
+  erb(:edit_list)
 end
