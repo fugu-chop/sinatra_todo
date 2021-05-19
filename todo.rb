0161496d0e344de4952de4f205bfefc194f6e63b
Dean Wan <deanwan14@gmail.com>
2021-05-19 18:44:50 +1000

    Add: Error Handling and validation

--- todo.rb
+++ todo.rb
@@ -15,6 +15,27 @@ before do
   session[:lists] ||= []
 end
 
+helpers do
+  def valid_input?
+    (1..100).cover?(@list_name.size)
+  end
+
+  def create_list
+    session[:lists] << { name: @list_name, todos: [] }
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+
+  def set_error_message
+    session[:error] = "The list name must be between 1 and 100 characters." if !valid_input?
+    session[:error] = "The list name must be unique." if duplicate_list_name?
+  end
+
+  def duplicate_list_name?
+    session[:lists].any? { |list| list[:name] == @list_name }
+  end
+end
+
 get "/" do 
   redirect "/lists"
 end
@@ -33,8 +54,9 @@ end
 # Create a new list
 post "/lists" do
   # params[:list_name] comes from the name of the input field in our erb file
-  # With a POST request, the name:value are captured as invisible query params within the repsonse body
-  session[:lists] << { name: params[:list_name], todos: [] }
-  session[:success] = "The list has been created."
-  redirect "/lists"
+  # With a POST request, the name:value are captured as invisible query params within the response body
+  @list_name = params[:list_name].strip
+  return create_list if valid_input? && !duplicate_list_name?
+  set_error_message
+  erb(:new_list)
 end
