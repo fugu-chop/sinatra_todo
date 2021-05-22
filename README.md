96781f9ba19621c6f0d48b2e85b8853b1a80fff0
Dean Wan <deanwan14@gmail.com>
2021-05-22 10:54:46 +1000

    Morning updates, 22nd May

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-# Sinatra To-Do
+# Sinatra To-Do App
 A repo to contain a web-based personal task manager using Sinatra.
 
-The app will be able to store data in the session, use HTTP methods other than `GET`, and cover basics on how to work with JavaScript in a Sinatra project.
+### Basic Overview
+This is a Sinatra based To-Do app that allows users to create multiple lists, where each list can contain multiple to-do items, all through the web browser. Both individual to-do items and lists can be created, renamed and deleted, with some validation built in (i.e. preventing duplicate lists, duplicate to-do items on the same list, empty names).
+
+Lists and to-dos are persisted using the `sessions` feature in Sinatra, which enables a cookie to be stored in the browser for usage between sessions.
+
+A layout was used to reduce the number of `.erb` view templates required. Most of the styling was provided as a CSS template file. 
+
+### How to run
+This project is currently still in development. To get it running locally:
+1. Clone the repo locally
+2. Run `bundle install` in your CLI
+3. Ruby `ruby todo.rb` in your CLI
+4. Visit `http://localhost:4567` in your web browser
+5. If you need to reset the app (i.e. delete all information), please delete the associated cookie through your browser.
+
+### Design Choices
+Many of the golden path 'verbs' within the app (e.g. creating a list, deleting a to-do) are extracted to methods. This causes some degree of code bloat, with a lot of methods being necessary for each individual action. Failure states _aren't_ extracted to methods, because 'failing' doesn't seem to match the equivalent of succeeding (e.g. `failed_create_list` versus `create_list`).
+
+I've chosen to reduce the amount of conditionals/`if` statements in my code on advice from other programmers. There's obviously a balance, but just as an exercise, I've tried to completely avoid `if/else` statements, which has necessitated another choice to do with routing.
+
+Routing for the 'golden' path is also handled within individual methods; I think this isn't best practice, since the act of 'updating a list' (for example) doesn't necessarily imply that a re-route should occur. However, I've chosen to do this because I have chosen to forgo `if` statements - within the requests within Sinatra, I need to `return` the golden path, as my fail path is directly below the golden path (i.e. displaying errors, other routing).
+
+### Challenges
+Avoiding `if` statements made me think about how to sequentially structure my code - `if` does provide a lot of convenience, but it was an interesting exercise. It meant I had to make sure to `return` certain methods, and that those methods did absolutely everything needed before being returned. In retrospect they probably do a little too much (usually they perform some action __as well as__ handle redirects).
+
+Working with both instance and local variables was a bit of a challenge. Because blocks create their own scope (i.e. for each request method in Sinatra), and routes are mutually exclusive, there's little risk of overwriting/conflicting instance variables. The lazy programmer in me just wants to use instance variables everywhere, as they allow access to the relev ant `.erb` template.
+
+### Project To Dos
+- More generic error message generation
--- todo.rb
+++ todo.rb
@@ -18,66 +18,64 @@ before do
   session[:lists] ||= []
 end
 
-helpers do
-  def valid_list_input?
-    (1..100).cover?(@list_name.size)
-  end
+def valid_list_input?
+  (1..100).cover?(@list_name.size)
+end
 
-  def create_list
-    session[:lists] << { name: @list_name, todos: [] }
-    session[:success] = "The '#{@list_name}' list has been created."
-    redirect '/lists'
-  end
+def create_list
+  session[:lists] << { name: @list_name, todos: [] }
+  session[:success] = "The '#{@list_name}' list has been created."
+  redirect '/lists'
+end
 
-  def update_list(id)
-    @list[:name] = @list_name
-    session[:success] = "The '#{@list[:name]}' list has been updated."
-    redirect "/lists/#{id}"
-  end
+def update_list(id)
+  @list[:name] = @list_name
+  session[:success] = "The '#{@list[:name]}' list has been updated."
+  redirect "/lists/#{id}"
+end
 
-  # I think this can be refactored to be universal. I need to make available the relevant variables
-  def set_list_error_message
-    session[:error] = 'The list name must be between 1 and 100 characters.' unless valid_list_input?
-    session[:error] = 'The list name must be unique.' if duplicate_list_name?
-  end
+# I think this can be refactored to be universal. I need to make available the relevant variables
+def set_list_error_message
+  session[:error] = 'The list name must be between 1 and 100 characters.' unless valid_list_input?
+  session[:error] = 'The list name must be unique.' if duplicate_list_name?
+end
 
-  def duplicate_list_name?
-    session[:lists].any? { |list| list[:name] == @list_name }
-  end
+def duplicate_list_name?
+  session[:lists].any? { |list| list[:name] == @list_name }
+end
 
-  # We want to handle the edge case where we want to enable users to rename their list the same existing name
-  def duplicate_list_name_except_current?(list_name)
-    remove_orig = session[:lists].reject { |list| list[:name] == list_name }
-    remove_orig.any? { |list| list[:name] == @list_name }
-  end
+# We want to handle the edge case where we want to enable users to rename their list the same existing name
+def duplicate_list_name_except_current?(list_name)
+  remove_orig = session[:lists].reject { |list| list[:name] == list_name }
+  remove_orig.any? { |list| list[:name] == @list_name }
+end
 
-  def delete_list(idx)
-    session[:lists].delete_at(idx)
-    session[:success] = "The '#{@list_name}' list has been deleted."
-    redirect "/lists"
-  end
+def delete_list(idx)
+  session[:lists].delete_at(idx)
+  session[:success] = "The '#{@list_name}' list has been deleted."
+  redirect '/lists'
+end
 
-  def create_todo(list_id)
-    @list[:todos] << { name: "#{params[:todo]}", completed: false }
-    session[:success] = "The '#{params[:todo]}' item was added to the list."
-    redirect "/lists/#{list_id}"
-  end
+def create_todo(list_id)
+  @list[:todos] << { name: params[:todo].to_s, completed: false }
+  session[:success] = "The '#{params[:todo]}' item was added to the '#{@list[:name]}' list."
+  redirect "/lists/#{list_id}"
+end
 
-  def duplicate_todo_input?
-    @list[:todos].any? { |list| list[:name] == params[:todo] }
-  end
+def duplicate_todo_input?
+  @list[:todos].any? { |list| list[:name] == params[:todo] }
+end
 
-  def valid_todo_input?
-    todo_name = params[:todo].strip
-    (1..100).cover?(todo_name.size)
-  end
+def valid_todo_input?
+  todo_name = params[:todo].strip
+  (1..100).cover?(todo_name.size)
+end
 
-  # Have created this since a lot of the instance variables required for set_list_error_message errors aren't available
-  # There are also dependencies in the template files to have access to instance variables for rendering
-  def set_todo_error_message
-    session[:error] = 'The todo name must be unique.' if duplicate_todo_input?
-    session[:error] = 'The todo name must be between 1 and 100 characters.' unless valid_todo_input?
-  end
+# Have created this since a lot of the instance variables required for set_list_error_message errors aren't available
+# There are also dependencies in the template files to have access to instance variables for rendering
+def set_todo_error_message
+  session[:error] = 'The todo name must be unique.' if duplicate_todo_input?
+  session[:error] = 'The todo name must be between 1 and 100 characters.' unless valid_todo_input?
 end
 
 get '/' do
@@ -106,20 +104,20 @@ end
 
 get '/lists/:id' do
   # Params are passed as strings from Sinatra
-  idx = params[:id].to_i
-  @list = session[:lists][idx]
+  @idx = params[:id].to_i
+  @list = session[:lists][@idx]
   erb(:list)
 end
 
 get '/lists/:id/edit' do
-  idx = params[:id].to_i
-  @list = session[:lists][idx]
+  @idx = params[:id].to_i
+  @list = session[:lists][@idx]
   erb(:edit_list)
 end
 
 post '/lists/:id' do
-  idx = params[:id].to_i
-  @list = session[:lists][idx]
+  @idx = params[:id].to_i
+  @list = session[:lists][@idx]
   original_name = @list[:name]
   @list_name = params[:list_name].strip
   return update_list(idx) if valid_list_input? && !duplicate_list_name_except_current?(original_name)
@@ -129,17 +127,16 @@ post '/lists/:id' do
 end
 
 post '/lists/:id/delete' do
-  idx = params[:id].to_i
-  @list_name = session[:lists][idx][:name]
-  delete_list(idx)
+  @idx = params[:id].to_i
+  @list_name = session[:lists][@idx][:name]
+  delete_list(@idx)
 end
 
 post '/lists/:list_id/todos' do
-  list_id = params[:list_id].to_i
-  @list = session[:lists][list_id]
-  # Needs more validation around blank todo
-  return create_todo(list_id) if !duplicate_todo_input? && valid_todo_input?
+  @idx = params[:list_id].to_i
+  @list = session[:lists][@idx]
+  return create_todo(@idx) if !duplicate_todo_input? && valid_todo_input?
 
   set_todo_error_message
-  redirect "/lists/#{list_id}"
+  erb(:list)
 end
