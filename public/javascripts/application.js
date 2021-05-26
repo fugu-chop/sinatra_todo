0fa99199f1b5aab562463b9892cb7aca01d9f57b
Dean Wan <deanwan14@gmail.com>
2021-05-27 08:31:19 +1000

    Add: jQuery functionality to delete lists and todos

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,22 @@
-console.log('hello!');
+// This tells jQuery to wait until all the elements in the document are fully rendered
+// The $ symbol is a reference to the jQuery object, which we pass a function
+$(function () {
+
+  // form.delete is the event listener - the event that should trigger event handlers (passed in as anonymous function)
+  // form.delete selects the class 'delete' from the 'form' elements
+  // .submit is the actual event - the submission of a 'form' element, with the 'delete' class
+  $("form.delete").submit(function (event) {
+    // These are event handlers, which execute some code when the requisite event occurs
+    // This stops the default behaviour from occuring, which would be the form being submitted
+    event.preventDefault();
+    // This stops the event from 'bubbling up' and being interpreted by oher parts of the code or the browser itself
+    event.stopPropagation();
+
+    var ok = confirm("Are you sure? This cannot be undone!");
+    if (ok) {
+      // Dynamically defines whichever element the event is being fired on
+      this.submit();
+    }
+  });
+
+});
